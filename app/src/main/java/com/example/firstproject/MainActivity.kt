b523c1d8ba423cdc11bb01538a061f4ce5ccfe31
BT_1N3_32 <sample@bths.edu>
2024-10-15 08:47:51 -0400

    made variables

--- app/src/main/java/com/example/firstproject/MainActivity.kt
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.firstproject
 
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val layout: LinearLayout = findViewById(R.id.main)
+        val scoreText: TextView = findViewById(R.id.score)
+        val strikeText: TextView = findViewById(R.id.strikes)
+        var score = 0
+        var strike = 0
+
 
     }
 }
