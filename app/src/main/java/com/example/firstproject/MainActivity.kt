5ecbd984ebf168202a4c8cc8c4fd5386142b2c1b
Caitlin Lee <caitlinlee@caitlins-air.mynetworksettings.com>
2024-10-16 02:36:50 -0400

    finished project ready for testing

--- app/src/main/java/com/example/firstproject/MainActivity.kt
+++ app/src/main/java/com/example/firstproject/MainActivity.kt
@@ -1,24 +1,151 @@
 package com.example.firstproject
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var layout: LinearLayout
+    private lateinit var scoreText: TextView
+    private lateinit var strikeText: TextView
+    private lateinit var num1Text: TextView
+    private lateinit var num2Text: TextView
+    private lateinit var button: Button
+    private lateinit var instructions: TextView
+
+    private var score: Int = 0
+    private var strike: Int = 0
+    private var num1: Int = 0
+    private var num2: Int = 0
+    private var gameStart: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*-----------------------------------------------*/
+        //Setting variables for each component in the game
+        layout = findViewById(R.id.main)
 
-        val layout: LinearLayout = findViewById(R.id.main)
-        val scoreText: TextView = findViewById(R.id.score)
-        val strikeText: TextView = findViewById(R.id.strikes)
-        var score = 0
-        var strike = 0
+        scoreText= findViewById(R.id.score)
+        strikeText = findViewById(R.id.strikes)
+        instructions = findViewById(R.id.instructions)
+        num1Text = findViewById(R.id.num1)
+        num2Text = findViewById(R.id.num2)
 
+        button = findViewById(R.id.button)
+        /*-----------------------------------------------*/
+        scoreText.text = "Score: " + score
+        strikeText.text = "Strikes: " + strike
+        gameStart = false;
+
+        button.setOnClickListener {
+            if (!gameStart){
+                gameStart = true
+                playGame()
+            } else {
+                /* Resets all values back to default/starting values */
+                gameStart = false
+                button.text = "START"
+                instructions.text = "Tap start to begin!"
+                num1Text.text = ""
+                num2Text.text = ""
+                score = 0
+                strike = 0
+                scoreText.setTextColor(Color.BLACK)
+                strikeText.setTextColor(Color.BLACK)
+                scoreText.text = "Score: " + score
+                strikeText.text = "Strike: " + strike
+                layout.setBackgroundColor(Color.parseColor("#FDEF74"))
+            }
+        }
 
     }
+
+    private fun playGame(){
+        button.text = "RESTART"
+        instructions.text = "Tap the larger number!"
+        setRandNum()
+
+        num1Text.setOnClickListener {
+            checkAnswer(num1, num2)
+            checkWin()
+        }
+
+        num2Text.setOnClickListener {
+            checkAnswer(num2, num1)
+            checkWin()
+        }
+    }
+
+    private fun checkWin(){
+        if (score == 10){
+            scoreText.setTextColor(Color.GREEN)
+            strikeText.setTextColor(Color.BLACK)
+            layout.setBackgroundColor(Color.parseColor("#FDEF74"))
+            scoreText.text = "Score: " + score
+            instructions.text = "Tap restart to play again!"
+            num1Text.text = ""
+            num2Text.text = ""
+            val winToast: Toast = Toast.makeText(this, "Congrats, you won! ^^", Toast.LENGTH_SHORT)
+            winToast.show()
+        } else if (strike == 3){
+            strikeText.setTextColor(Color.RED)
+            scoreText.setTextColor(Color.BLACK)
+            layout.setBackgroundColor(Color.parseColor("#FDEF74"))
+            strikeText.text = "Strikes: " + strike
+            instructions.text = "Tap restart to play again!"
+            num1Text.text = ""
+            num2Text.text = ""
+            val loseToast: Toast = Toast.makeText(this, "Sorry, you lost :(", Toast.LENGTH_SHORT)
+            loseToast.show()
+        } else {
+            //Nothing
+        }
+    }
+
+    private fun checkAnswer(first: Int, second: Int){
+        if (first > second){
+            incrementScore()
+            layout.setBackgroundColor(Color.GREEN)
+            setRandNum()
+        } else {
+            incrementStrike()
+            layout.setBackgroundColor(Color.RED)
+            setRandNum()
+        }
+    }
+
+    private fun setRandNum() {
+        num1 = Random.nextInt(101)
+        num2 = Random.nextInt(101)
+        while (num1 == num2){
+            num1 = Random.nextInt(101)
+        }
+        num1Text.text = num1.toString()
+        num2Text.text = num2.toString()
+    }
+
+    private fun incrementScore(){
+        score++;
+        scoreText.setTextColor(Color.YELLOW)
+        strikeText.setTextColor(Color.BLACK)
+        scoreText.text = "Score: " + score
+    }
+
+    private fun incrementStrike(){
+        strike++;
+        strikeText.setTextColor(Color.YELLOW)
+        scoreText.setTextColor(Color.BLACK)
+        strikeText.text = "Strikes: " + strike
+    }
+
 }
